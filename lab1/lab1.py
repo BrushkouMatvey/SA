fe678a77aeff8d94a62c22c4f5c8277b7d0a0975
BrushkouMatvey <brushkoumatvey@gmail.com>
2020-02-09 17:06:08 +0300

    Add multiple correlation coefficient

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -3,13 +3,19 @@ import pandas as pd  # To read data
 import matplotlib.pyplot as plt  # To visualize
 from sklearn.linear_model import LinearRegression
 
-df = pd.read_excel('Faults.xlsx')  # load data set
-X = df.iloc[:, 14].values.reshape(-1, 1)  # values converts it into a numpy array
-Y = df.iloc[:, 15].values.reshape(-1, 1)  # -1 means that calculate the dimension of rows, but have 1 column
-linear_regressor = LinearRegression()  # create object for the class
-linear_regressor.fit(X, Y)  # perform linear regression
-Y_pred = linear_regressor.predict(X)  # make predictions
+data = pd.read_excel('Faults.xlsx')  # load data set
 
-plt.scatter(X, Y)
-plt.plot(X, Y_pred, color='red')
+Edges_Index_X = data.iloc[:, 14].values.reshape(-1, 1)# values converts it into a numpy array
+Empty_Index_Y = data.iloc[:, 15].values.reshape(-1, 1)  # -1 means that calculate the dimension of rows, but have 1 column
+
+linear_regressor = LinearRegression()  # create object for the class
+linear_regressor.fit(Edges_Index_X, Empty_Index_Y)  # perform linear regression
+Y_pred = linear_regressor.predict(Edges_Index_X)  # make predictions
+
+plt.scatter(Edges_Index_X, Empty_Index_Y)
+plt.plot(Edges_Index_X, Y_pred, color='red')
+plt.xlabel('Edges_Index')
+plt.ylabel('Empty_Index')
+
+print(data.iloc[:, [14, 15]].corr())
 plt.show()
